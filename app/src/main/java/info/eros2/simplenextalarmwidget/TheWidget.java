45a54d695ebfa1af572e48864c1d895d703b17dc
eugene-rom <eros2@inbox.ru>
2018-02-05 16:55:23 +0200

    open clock app on tap

--- app/src/main/java/info/eros2/simplenextalarmwidget/TheWidget.java
+++ app/src/main/java/info/eros2/simplenextalarmwidget/TheWidget.java
@@ -1,53 +1,58 @@
 package info.eros2.simplenextalarmwidget;
 
-import android.annotation.SuppressLint;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.provider.AlarmClock;
 import android.provider.Settings;
 import android.widget.RemoteViews;
 
 public class TheWidget extends AppWidgetProvider
 {
-    private static void updateAppWidget( Context context, AppWidgetManager appWidgetManager, int appWidgetId )
+    private static final String TAP_INTENT = "info.eros2.simplenextalarmwidget.TAP_WIDGET";
+
+    @Override
+    public void onUpdate( Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds )
     {
+        PendingIntent pendingIntent = PendingIntent.getBroadcast( context, 0,
+                                new Intent( TAP_INTENT ), PendingIntent.FLAG_UPDATE_CURRENT );
+
         @SuppressWarnings("deprecation")
         String nextAlarm = Settings.System.getString( context.getContentResolver(),
-                                                        Settings.System.NEXT_ALARM_FORMATTED );
+                Settings.System.NEXT_ALARM_FORMATTED );
 
         if ( ( nextAlarm == null ) || nextAlarm.isEmpty() ) {
-            nextAlarm = "-- --:--";
+            nextAlarm = context.getString( R.string.appwidget_text );
         }
 
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews( context.getPackageName(), R.layout.widget );
         views.setTextViewText( R.id.appwidget_text, nextAlarm );
+        views.setOnClickPendingIntent( R.id.appwidget_text, pendingIntent );
 
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget( appWidgetId, views );
-    }
-
-    @Override
-    public void onUpdate( Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds ) {
         // There may be multiple widgets active, so update all of them
         for ( int appWidgetId : appWidgetIds ) {
-            updateAppWidget( context, appWidgetManager, appWidgetId );
+            appWidgetManager.updateAppWidget( appWidgetId, views );
         }
     }
 
-    @SuppressLint("UnsafeProtectedBroadcastReceiver")
     @Override
     public void onReceive( Context context, Intent intent )
     {
-        //if ( intent.getAction().equals( "android.app.action.NEXT_ALARM_CLOCK_CHANGED" ) ) {
-            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance( context );
-            ComponentName thisAppWidget = new ComponentName( context.getPackageName(), getClass().getName() );
-            int[] appWidgetIds = appWidgetManager.getAppWidgetIds( thisAppWidget );
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance( context );
+        ComponentName thisAppWidget = new ComponentName( context.getPackageName(), getClass().getName() );
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds( thisAppWidget );
 
-            onUpdate( context, appWidgetManager, appWidgetIds );
-        //}
+        onUpdate( context, appWidgetManager, appWidgetIds );
+
+        if ( TAP_INTENT.equals( intent.getAction() ) ) {
+            Intent openClockIntent = new Intent( AlarmClock.ACTION_SHOW_ALARMS );
+            openClockIntent.setFlags( Intent.FLAG_ACTIVITY_NEW_TASK );
+            context.startActivity( openClockIntent );
+        }
     }
 
     @Override
